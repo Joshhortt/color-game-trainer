940911e0e2788541e80aadaf6fffefd0cb9f0818
Jose Horta <joshhortt@yahoo.com>
2019-04-29 19:02:49 +0100

    add Eventhandler to every single square; add variable clickedColors and .this method.

--- scripts.js
+++ scripts.js
@@ -14,6 +14,13 @@ var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
+     // add initial colors to squares
 			squares[i].style.background = colors[i];
 
+      //add click listeners to squares
+  squares[i].addEventListener("click", function(){
+  // grab color of clicke squares
+  var clickedColor = this.style.background;
+  // compare color to pickedColor
+  });
     }
