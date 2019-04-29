9900e9bda54aa38efea8317dbac1ad0bc71a9785
Jose Horta <joshhortt@yahoo.com>
2019-04-29 18:45:08 +0100

    add span' to h1' with an id' and the variable that getElementById' in js file', add also variable to pick 3 colors.

--- scripts.js
+++ scripts.js
@@ -5,11 +5,13 @@ var colors = [
   "rgb(0, 255, 0)",
   "rgb(0, 255, 255)",
   "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)",
+  "rgb(255, 0, 255)"
 ]
 
 var squares = document.querySelectorAll(".square");
-
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
 for(var i = 0; i < squares.length; i++){
 			squares[i].style.background = colors[i];
+
     }
