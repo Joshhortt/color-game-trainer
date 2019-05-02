a4b5c7bfa0653e666494807d8c87c0e36f83c3d0
Jose Horta <joshhortt@yahoo.com>
2019-05-02 12:46:47 +0100

    add method' to variable pickedColor'; add new function' to pick arandom number using .Math method' of an array [3]'.

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 
@@ -41,3 +41,8 @@ for(var i = 0; i < squares.length; i++){
   		squares[i].style.background = color;
   	}
   }
+
+  function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
